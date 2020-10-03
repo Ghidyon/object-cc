bc20af1f00b63b2581cc13fff6c074e30c290f2e
Ghidyon <gideonakunana@gmail.com>
2020-10-03 15:04:40 +0100

    implemented ES6

--- script.js
+++ script.js
@@ -10,24 +10,28 @@ function Cart() {
             "quantity": quantity,
             "price": price
         }
-
-        // Conditions to check if item is already in the basket
-        if (basket.length > 0) {
-
-            const checkItems = (element) => {
-                return element.itemName === itemName;
-            };
-
+        // Function to check if item to be added is already in the cart
+        const checkItems = (element) => {
+            return element.itemName === itemName;
+        }
+        // Function to add an item
+        const addsItem = () => {
             const itemRepeated = basket.some(checkItems);
 
             if (itemRepeated) {
                 return console.log("This item has been added previously, try updating your cart.");
             }
-
             else {
                 basket.push(newItem);
                 return console.log(basket);
             }
+        }
+
+
+
+        // Conditions to check if item is already in the basket
+        if (basket.length > 0) {
+            addsItem();
         }
 
         else {
@@ -35,74 +39,88 @@ function Cart() {
             return console.log(basket);
         }
     };
-    
+
     this.updateItem = (itemName, parameter, value) => {
-      if (basket.length > 0) {
-        basket.forEach((element) => {
-          if (element.itemName === itemName) {
-              let indexValue = basket.indexOf(element);
-              let found = basket[indexValue];
-              if (parameter === 'quantity') {
-                  found.quantity = value;
-                  return console.log(found);
-              } else if (parameter === 'price') {
-                  found.price = value;
-                  return console.log(found);
-              } else {
-                  return console.log(`Parameter doesn't exist`);
-              }
-          }
-        })
-      }
-      
-      else {
-          return console.log(`Cart is empty!`);
-      }
-    }
-    
-    this.removeItem = (itemName, quantity) => {
-        if (basket.length > 0) {
-            
-            basket.forEach((element) => {
-              
-                if (element.itemName === itemName) {
-                      const indexValue = basket.indexOf(element);
-                      const found = basket[indexValue];
-                      if (quantity === found.quantity) {
-                            basket.splice(indexValue, 1);
-                            return console.log(basket);
-                      }
-                      else if (quantity < found.quantity) {
-                          const newQuantity = found.quantity - quantity;
-                          found.quantity = newQuantity;
-                          return console.log(basket);
-                      }
-                      else {
-                          return console.log(`Error! Quantity of items exceeded.`);
-                      }
+
+        // Function to update an existing item
+        const updatesItem = (element) => {
+            if (itemName === element.itemName) {
+                let indexValue = basket.indexOf(element);
+                let itemFound = basket[indexValue];
+
+                if (parameter === 'quantity') {
+                    itemFound.quantity = value;
+                    return console.log(itemFound);
                 }
-            })
+                else if (parameter === 'price') {
+                    itemFound.price = value;
+                    return console.log(itemFound);
+                }
+                else {
+                    return console.log(`Parameter doesn't exist`);
+                }
+            }
         }
-        
+
+        // Conditions to check item to be updated
+        if (basket.length > 0) {
+            basket.forEach(updatesItem)
+        }
+        else {
+            return console.log(`Cart is empty!`);
+        }
+    }
+
+    this.removeItem = (itemName, quantity) => {
+
+        // Function to remove an item
+        const removesItem = (element) => {
+            if (element.itemName === itemName) {
+                const indexValue = basket.indexOf(element);
+                const itemFound = basket[indexValue];
+
+                if (quantity === itemFound.quantity) {
+                    basket.splice(indexValue, 1);
+                    return console.log(basket);
+                }
+                else if (quantity < itemFound.quantity) {
+                    const newQuantity = itemFound.quantity - quantity;
+                    itemFound.quantity = newQuantity;
+                    return console.log(basket);
+                }
+                else {
+                    return console.log(`Error! Quantity of items exceeded.`);
+                }
+            }
+        }
+
+        // Conditions to check item to be removed
+        if (basket.length > 0) {
+            basket.forEach(removesItem);
+        }
         else {
             return console.log('Cart is empty!');
         }
     }
-    
+
     this.total = () => {
+        let sumOfItems = 0;
+
+        // Function to get total of all items
+        const getTotal = (element) => {
+            const itemTotal = element.price * element.quantity;
+            sumOfItems += itemTotal;
+        }
+
+        // Conditions to check total number of items
         if (basket.length > 0) {
-            let sumOfItems = 0;
-            basket.forEach((element) => {
-                const itemTotal = element.price * element.quantity;
-                sumOfItems += itemTotal;
-            })
+            basket.forEach(getTotal);
             return console.log(sumOfItems);
-        } 
+        }
         else {
             return console.log("Cart is empty!")
         }
     }
-
 }
 
 const item = new Cart();
@@ -112,7 +130,7 @@ item.addItem("mango", 5, 200);
 item.addItem("goman", 5, 200);
 item.addItem("beans", 5, 200);
 item.addItem("seand", 5, 200);
-item.updateItem("beans", 'price', 500);
+item.updateItem("beans", 'price', 200);
 item.removeItem("beans", 9);
 item.removeItem("mango", 3);
 item.total();
